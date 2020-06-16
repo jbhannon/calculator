8b18c347c6d387d13e5d924409eb27951c349a55
Jordan Hannon <JordanBHannon@gmail.com>
2020-06-16 03:51:44 -0400

    Complete main calculator functionality

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,39 +1,100 @@
 const add = (a, b) => a + b;
-			const subtract = (a, b) => a - b;
-			const multiply = (a, b) => a * b;
-			const divide = (a, b) => a / b;
-			const operate = (op) => op(lastNum, currentNum);
-			const display = () => (document.querySelector("#display").textContent = displayStr);
-            function chooseOperation(){
-                switch (operation) {
-                    case "add":
-                        operate(add);
-                        return;
-                    case "subtract":
-                        operate(sub);
-                        return;
-                    case "multiply":
-                        operate(multiply);
-                        return;
-                    case "divide":
-                        operate(divide);
-                        return;
-                    
-                }
-            }
-			let displayStr = "";
-            let currentNum = 0;
-            let lastNum = 0;
-            let operation = "";
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const operate = (op, a, b) => {
+    if (op == divide && b == 0) {
+        clear();
+        document.querySelector("#display").textContent = "pls don't";
+        broken = true;
+        return;
+    }
+    return op(a, b);
+}
+const display = (str) => {
+    if (str.toString().length > 10 && str.toString().includes(".")) str = str.toString().slice(0,str.toString().indexOf(".") + 4)
+    document.querySelector("#display").textContent = str;
+}
+const clear = () => {
+    operation = null;
+    savedNum = 0;
+    workingStr = "";
+    display(workingStr);
+}
+const back = () => {
+    if (workingStr) {
+        workingStr = workingStr.slice(0, workingStr.length-1);
+    }
+    display(workingStr);
+}
+let broken = false;
+let operation = null;
+let savedNum = 0;
+let workingStr = "";
 
-			document.querySelectorAll(".number").forEach((button) => {
-				button.addEventListener("click", (e) => {
-					displayStr += `${e.target.getAttribute("data-do")}`;
-					display();
-				});
-			});
-            document.querySelectorAll(".operator").forEach((button) => {
-				button.addEventListener("click", (e) => {
-					operation = e.target.getAttribute("data-do");
-				});
-			});
+
+
+document.querySelectorAll(".number").forEach((button) => {
+    button.addEventListener("click", (e) => {
+        let char = e.target.getAttribute("data-do");
+        if (char == "." && workingStr.includes(".")) {
+            return;
+        }
+        workingStr += char;
+        display(workingStr);
+    });
+});
+
+document.querySelectorAll(".operator").forEach((button) => {
+    button.addEventListener("click", (e) => {
+        let operator = e.target.getAttribute("data-do");
+        if (operation && operator == "equals") {
+            workingStr = operate(operation, savedNum, +workingStr);
+            if (broken){
+                broken = false;
+                workingStr = "";
+                return;
+            }
+            operation = null;
+            savedNum = +workingStr;
+            workingStr = "";
+            display(savedNum);
+            return;
+        }
+        if(operation) {
+            workingStr = operate(operation, savedNum, +workingStr);
+            if (broken) {
+                broken = false;
+                workingStr ="";
+                return;
+            }
+            savedNum = 0;
+            operation = null;
+            display(workingStr);
+        }
+        switch (operator) {
+            case "add":
+                operation = add;
+                break;
+            case "subtract":
+                operation = subtract;
+                break;
+            case "multiply":
+                operation = multiply;
+                break;
+            case "divide":
+                operation = divide;
+                break;
+            default:
+                return;
+        }
+        if (workingStr) {
+            savedNum = +workingStr;
+            workingStr = "";
+            display(savedNum);
+        }
+    });
+});
+
+document.querySelector("#clear").addEventListener("click", clear);
+document.querySelector("#back").addEventListener("click", back);
